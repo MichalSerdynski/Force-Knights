0715633bab6eee8276ea4e91d64374589c77e1fd
Shinradawn <99540580+Shinradawn@users.noreply.github.com>
2023-04-05 13:34:36 +0100

    door
    Door opens when you pick up keycard,
    Door is impassable until it opens

--- Assets/Force Knights Map Assets/Objects/Scripts/DestroyOnTrigger.cs
+++ Assets/Force Knights Map Assets/Objects/Scripts/DestroyOnTrigger.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class DestroyOnTrigger : MonoBehaviour
 {
     public GameObject objToDestroy;
-
-    void OnTriggerEnter(Collider other)
+    public GameObject doorToOpen;
+    private Animator otherAnimator;
+    private Collider2D otherCollider;
+    void Start()
+    {
+        otherCollider = doorToOpen.GetComponent<BoxCollider2D>();
+        otherAnimator = doorToOpen.GetComponent<Animator>();
+    }
+        void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
             Destroy(objToDestroy);
+        
+        otherAnimator.SetBool("HasKey", true);
+        otherCollider.isTrigger = true;
     }
 }
