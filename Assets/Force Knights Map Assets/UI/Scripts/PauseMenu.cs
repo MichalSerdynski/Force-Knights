df62ca0e9d93a7cb529e554f49c19f5aed538547
Shinradawn <99540580+Shinradawn@users.noreply.github.com>
2023-04-14 12:59:13 +0100

    Basic Pause menu added
    Pause menu with ESC resume and quit working

---
+++ Assets/Force Knights Map Assets/UI/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public static bool gameIsPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("YourGameSceneName");
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void SaveGame()
+    {
+        // Add your save game logic here
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
